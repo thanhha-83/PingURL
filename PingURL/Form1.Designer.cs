04d6040c693e931b55e085cba4f4b54b1fd1bbe8
FKMAGENCY\ha.phan <ha.phan@9thwonder.com>
2023-01-08 20:08:43 +0700

    init app

---
+++ PingURL/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PingURL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lbFile = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dtgvResult = new System.Windows.Forms.DataGridView();
+            this.Col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose file:";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(107, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(99, 33);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lbFile
+            // 
+            this.lbFile.AutoSize = true;
+            this.lbFile.Location = new System.Drawing.Point(212, 18);
+            this.lbFile.MaximumSize = new System.Drawing.Size(250, 21);
+            this.lbFile.Name = "lbFile";
+            this.lbFile.Size = new System.Drawing.Size(0, 21);
+            this.lbFile.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 80);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 32);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Ping";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dtgvResult
+            // 
+            this.dtgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col1,
+            this.Col2,
+            this.Col3});
+            this.dtgvResult.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dtgvResult.Location = new System.Drawing.Point(523, 0);
+            this.dtgvResult.Name = "dtgvResult";
+            this.dtgvResult.Size = new System.Drawing.Size(642, 616);
+            this.dtgvResult.TabIndex = 4;
+            // 
+            // Col1
+            // 
+            this.Col1.HeaderText = "URL";
+            this.Col1.Name = "Col1";
+            this.Col1.Width = 300;
+            // 
+            // Col2
+            // 
+            this.Col2.HeaderText = "Status";
+            this.Col2.Name = "Col2";
+            // 
+            // Col3
+            // 
+            this.Col3.HeaderText = "DateTime Ping";
+            this.Col3.Name = "Col3";
+            this.Col3.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 616);
+            this.Controls.Add(this.dtgvResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbFile);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping URL";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lbFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dtgvResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col3;
+    }
+}
+
---
+++ PingURL/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingURL
+{
+    public partial class Form1 : Form
+    {
+        private string[] listURL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    lbFile.Text = filePath;
+                    using (Stream str = openFileDialog1.OpenFile())
+                    {
+                        listURL = File.ReadAllLines(filePath);
+                    }
+                    MessageBox.Show("Read successful");
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listURL)
+            {
+                string result = await PingAsync(item) ? "OK" : "Not Found";
+                dtgvResult.Rows.Add(item, result, DateTime.Now.ToString());
+            }
+        }
+        private static async Task<bool> PingAsync(string url)
+        {
+            Ping ping = new Ping();
+            PingReply result = await ping.SendPingAsync(url);
+            return result.Status == IPStatus.Success;
+        }
+    }
+}
